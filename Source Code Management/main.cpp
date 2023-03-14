98426bf25d112ff89384d5ff34841ea57a8205c0
Alex Rudy <arudy547@gmail.com>
2023-03-14 14:49:03 -0400

    Initial Commit

---
+++ Source Code Management/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Source Code Management
+//
+//  Created by Alex Rudy on 3/14/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
